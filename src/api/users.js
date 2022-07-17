534a09aa2e1d1c241c6a519b4ce2d4fd5c775683
Kritish Bhattarai <rishi58rishi@gmail.com>
2022-07-17 13:46:09 +0545

    feat: dashboard info

--- src/api/users.js
+++ src/api/users.js
@@ -4,3 +4,8 @@ export const getUsers = async () => {
   const res = await axios.get("/api/profile/admin/user-list/");
   return res.data.data.results;
 };
+
+export const getEndUser = async () => {
+  const response = await axios.get("/api/profile/");
+  return response.data.data;
+};
